f8bd3fc8dff53306f7411a6214cd7653512b0a80
Andrew Brehaut <andrew@brehaut.net>
2012-05-29 10:22:24 +1200

    amending copyright statement in source

--- color.js
+++ color.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2008, Andrew Brehaut, Tim Baumann
+// Copyright (c) 2008-2010, Andrew Brehaut, Tim Baumann, Matt Wilson
 // All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without
